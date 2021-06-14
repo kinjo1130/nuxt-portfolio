1d1418af1cbfd0c19763beb6317abaa403705180
AFURO-1130 <softtennis6161130@icloud.com>
2021-06-14 16:09:06 +0900

    2

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '',
-    title: 'KINJYO SHOTARO’s portfolio',
+    title: '',
     htmlAttrs: {
       lang: 'ja',
       prefix: 'og: http://ogp.me/ns#'
