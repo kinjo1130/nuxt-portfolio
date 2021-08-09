8ba5f97a523d3e0f59371d15c81c75b6921f17ab
AFURO-1130 <73173075+AFURO-1130@users.noreply.github.com>
2021-08-09 16:52:12 +0900

    コンタクトページ変更

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,12 +10,13 @@
                 <div class="d-flex align-center mx-4">Kinjyo Shotaro</div>
                 <v-spacer> </v-spacer>
                 <div class="d-flex align-center">
-                 <v-btn
+                  <v-btn
                     outlined
                     :color="theme ? 'orange' : 'yellow'"
                     @click="theme = !theme"
                     :prepend-icon="themeIcon"
-                  ><v-icon>{{ themeIcon }}</v-icon></v-btn>
+                    ><v-icon>{{ themeIcon }}</v-icon></v-btn
+                  >
                 </div>
               </v-col>
             </v-row>
@@ -28,11 +29,11 @@
                 <v-sheet min-height="70vh" rounded="lg">
                   <v-row class="d-flex justify-center">
                     <v-col cols="10" xl="10" lg="10" md="10" sm="10">
-                         <nuxt-link to="/" class="mb-3">トップへ戻る</nuxt-link>
+                      <nuxt-link to="/" class="mb-3">トップへ戻る</nuxt-link>
                       <v-form
                         action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSedrZU2MbBAIMYOUSC1luOrhdaLBLEKtj4ACQhqOXuYSjnT9w/formResponse"
                         method="POST"
-                        @submit="submitted=true;"
+                        @submit="submitted = true"
                         lazy-validation
                         target="hidden_iframe"
                         ref="test_form"
@@ -65,7 +66,7 @@
                             type="submit"
                             outlined
                             color="blue"
-                            @click="submit()"
+                            @click="submit"
                             >送信</v-btn
                           >
                         </div>
@@ -96,15 +97,15 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-    
+
   data() {
     return {
      theme: true,
      name:"",
      email:"",
      messages:"",
-    
-     
+
+    success: false,
     required: value => !!value || "必ず入力してください",
       submitted: false
 
@@ -121,18 +122,15 @@ export default Vue.extend({
     }
   },
   methods :{
-      submit(){
-      if (this.$refs.test_form.validate()){
-        this.success = true;
-      } else {
-        this.success = false;
-         
-      }
-      
-          
+      submit(e){
+        if(!this.$refs.test_form.validate()) {
+          e.preventDefault();
+        } else {
+          this.success = true;
+        }
       }
   }
-  
- 
+
+
 })
 </script>
