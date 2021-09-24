246c49dcce7b38d183c39a5c4697dfc74078279d
AFURO-1130 <73173075+AFURO-1130@users.noreply.github.com>
2021-09-24 20:09:07 +0900

    自己紹介の変更

--- pages/about.vue
+++ pages/about.vue
@@ -47,9 +47,9 @@
                       <v-divider></v-divider>
                       <span>英検4級</span>
                       <span>普通自動車免許</span>
-                      <span class="text-h5 mt-5">賞</span>
-                      <v-divider></v-divider>
-                      <span>EPSON ハッカソン　優勝</span>
+                      <!-- <span class="text-h5 mt-5">賞</span>
+                      <v-divider></v-divider> -->
+
                     </v-col>
                   </v-row>
                 </v-sheet>
--- pages/skills.vue
+++ pages/skills.vue
@@ -48,14 +48,14 @@
                        <span class="text-h6">Framework</span>
                       <v-divider></v-divider>
                       <span class="mb-5">
-                        vue.js / Nuxt.js / Vuetify / Bootstrap 
+                        vue.js / Nuxt.js / Vuetify / Bootstrap
                       </span>
                       <span class="text-h6">Other</span>
                       <v-divider></v-divider>
                       <span class="mb-5">
-                        Google App Script  / github 
+                        Google App Script  / github
                       </span>
-                      <span>最近興味あるのは、TypeScriptとfirebaseかAWSなどのインフラ技術とnode以外のバックエンド技術です。(欲張りすぎた笑)</span>
+                      <span>最近興味あるのは、TypeScriptとfirebaseかAWSなどのインフラ技術とnode以外のバックエンド技術です。</span>
                     </v-col>
                   </v-row>
                 </v-sheet>
@@ -79,7 +79,7 @@ export default Vue.extend({
   data() {
     return {
       theme: true,
-    
+
     }
   },
   computed: {
